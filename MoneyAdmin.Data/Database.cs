0dc10367e819719fb8744a29f4334ee4c96c7687
anyeloamt <anyeloamt@gmail.com>
2014-12-18 09:59:04 -0400

    Added Database context

---
+++ MoneyAdmin.Data/Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyAdmin.Data
+{
+    public class Database : DbContext
+    {
+        public Database() : 
+            base(ConfigurationManager.ConnectionStrings["MoneyAdminConn"].ConnectionString)
+        {
+            
+        }
+
+        public Database(string connectionString) :
+            base(connectionString)
+        {
+
+        }
+    }
+}
