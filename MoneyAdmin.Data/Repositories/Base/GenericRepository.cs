543a301d8cd26e45c949eef2a6a8cb3a39adb5d3
Anyelo Almánzar Mercedes <anyeloamt@gmail.com>
2014-12-21 10:08:02 -0400

    Moved repositoy to Data layer

--- MoneyAdmin.Data/Repositories/Base/GenericRepository.cs
+++ MoneyAdmin.Data/Repositories/Base/GenericRepository.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace GenericRepository
+namespace MoneyAdmin.Data.Repositories.Base
 {
     abstract public class GenericRepository<T, TC>
         : IGenericRepository<T>
--- MoneyAdmin.Data/Repositories/Base/IGenericRepository.cs
+++ MoneyAdmin.Data/Repositories/Base/IGenericRepository.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace GenericRepository
+namespace MoneyAdmin.Data.Repositories.Base
 {
     public interface IGenericRepository<T> where T : class
     {
