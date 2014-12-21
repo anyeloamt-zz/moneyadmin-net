eff46f90d93daa997da05b61f47d0ed8764ba4e3
Anyelo Almánzar Mercedes <anyeloamt@gmail.com>
2014-12-20 20:22:48 -0400

    Added repository pattern to project

---
+++ GenericRepository/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRepository
+{
+    abstract public class GenericRepository<T, TC>
+        : IGenericRepository<T>
+        where T : class
+        where TC : DbContext, new()
+    {
+        private TC _entities = new TC();
+
+        public TC Context
+        {
+            get { return _entities; }
+            set { _entities = value; }
+        }
+
+        public virtual IQueryable<T> AsQueryable()
+        {
+            return _entities.Set<T>();
+        }
+
+        public virtual IQueryable<T> FindBy(Expression<Func<T, bool>> conditions)
+        {
+            return _entities.Set<T>().Where(conditions);
+
+        }
+
+        public virtual IGenericRepository<T> Add(T entity)
+        {
+            _entities.Set<T>().Add(entity);
+            return this;
+        }
+
+        public virtual IGenericRepository<T> Update(T entity)
+        {
+            _entities.Entry(entity).State = EntityState.Modified;
+            return this;
+        }
+
+        public virtual IGenericRepository<T> Delete(T entity)
+        {
+            _entities.Entry(entity).State = EntityState.Deleted;
+            return this;
+        }
+
+        public virtual void Save()
+        {
+            _entities.SaveChanges();
+        }
+    }
+}
---
+++ GenericRepository/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> AsQueryable();
+
+        IQueryable<T> FindBy(Expression<Func<T, bool>> conditions);
+
+        IGenericRepository<T> Add(T entity);
+
+        IGenericRepository<T> Update(T entity);
+
+        IGenericRepository<T> Delete(T entity);
+
+        void Save();
+    }
+}
