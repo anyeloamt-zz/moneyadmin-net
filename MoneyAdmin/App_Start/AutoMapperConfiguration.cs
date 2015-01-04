82609d26584fef28c61280116399d674dac2baef
anyeloamt <anyeloamt@gmail.com>
2015-01-04 16:48:18 -0400

    Added missing files

---
+++ MoneyAdmin/App_Start/AutoMapperConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using MoneyAdmin.Data.Entities;
+using MoneyAdmin.ViewModel;
+
+namespace MoneyAdmin
+{
+	public static class AutoMapperConfiguration
+	{
+        public static IMappingExpression<TTo, TFrom> Back<TFrom, TTo>(this IMappingExpression<TFrom, TTo> expression)
+	    {
+	        return Mapper.CreateMap<TTo, TFrom>();
+	    }
+
+	    public static void Configure()
+	    {
+	        Mapper.CreateMap<User, UserViewModel>().Back();
+	    }
+	}
+}
