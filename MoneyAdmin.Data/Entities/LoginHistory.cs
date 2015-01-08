a156a482a08606577e1e7009c5b81d4074cf8a7b
anyeloamt <anyeloamt@gmail.com>
2015-01-07 21:38:20 -0400

    Added LoginHistory entity

---
+++ MoneyAdmin.Data/Entities/LoginHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyAdmin.Data.Entities
+{
+    public class LoginHistory
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
