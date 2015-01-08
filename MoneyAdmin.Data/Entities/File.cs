2aec03c00e5c8dc80b347e78c5cfb4cdc005eb02
anyeloamt <anyeloamt@gmail.com>
2015-01-07 21:39:55 -0400

    Added File entity

---
+++ MoneyAdmin.Data/Entities/File.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyAdmin.Data.Entities
+{
+    public class File : BaseEntity
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Path { get; set; }
+    }
+}
